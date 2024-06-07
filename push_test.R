e119138d71ff52ae09e53e70d84d4600923ceabd
Vian Tran <viantran@Vians-MacBook-Air.local>
2024-06-07 18:09:51 -0400

    Initial commit

---
+++ push_test.R
@@ -0,0 +1,20 @@
+#' this is a test script
+#' multi-line command using #'
+#' Then save this as push_test.R
+
+# it would have been more professional to use parameter names
+myvector <- seq(100, 1, by=-7)
+
+myavg <- mean(myvector)
+myavg
+
+#source runs all code - shift, command, S
+
+mysum <- sum(myvector)
+mysum
+
+#standard deviation
+mysd <- sd(myvector)
+mysd
+
+#dont use spaces EVER in naming your files, its _
