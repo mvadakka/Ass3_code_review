94e6bbd03b2f9f082f352e8ebb659604b057baac
Vian Tran <viantran@Vians-MacBook-Air.local>
2024-06-30 00:40:46 -0400

    playing around with the code

--- Assignment 3 - Hangman.R
+++ Assignment 3 - Hangman.R
@@ -15,29 +15,47 @@
 #4. Inform the user on the length of the secret word. Hint: You may test nchar()
 #5. Inform the user about the number of wrong guesses/tries allowed. 
 #Decide on the rule here and implement it.
-#while loop (condition - when wrong guesses < max wrong guess then continue with loop) 
-#break end it when wrong guesses = max wrong guess = game over
-#6. Ask for user input. The user is expected to enter one character only.
-#### may or may not need repeat loop - should be okay with just while loop ####
-#repeat loop - prompt for user inputs
-#7. Check System for one character 
-#within the repeat loop
-  #check if the character is a letter
-  #make sure that both lower and upper case letters are acceptable and treated as equivalent
+
+#While loop (condition - when wrong guesses < max wrong guess then continue with loop) 
+#Break: end it when wrong guesses = max wrong guess = game over OR guess word = secret word
+#Notify user that they’ve lost. Reveal secret and exit. OR Notify they won! 
+
+#Text to display current state of the game for users.
+
+#6. Ask for user input.
+#7a. Check System for one character (IF)
+  #Check if the character is a letter
+  #Make sure that both lower and upper case letters are acceptable and treated as equivalent
 #SIDE NOTE - create a vector to keep track of correctly guessed letters
-#8. Check to see if the user input is in the secret word. (correct)
+#8a. Check to see if the user input is in the secret word. (correct)
   #If yes, notify user and ask for next letter or guess the whole word
       #Always notify the user about the correct letters/wrong letters, remaining tries.
       #Provide the user with a visual clue of how they are progressing - update _ _ _ _ with 
       #correct guessed letters 
-      #repeat
   #If not, notify user.
       #If user has not exhausted the available tries repeat.
       #Always notify the user about the correct letters/wrong letters, remaining tries.
       #Provide the user with a visual clue of how they are progressing - update _ _ _ _ with 
       #correct guessed letters 
-      #OR
-      #If tries are exhausted, notify user that they’ve lost. Reveal secret and exit.
+      #OR 
+      #If tries are exhausted, notify user that they’ve lost. Reveal secret and exit. (This 
+      #will bring it back to the while loop which will break when # of wrong guesses = max 
+      #wrong guesses)
+
+#7b. Check system if user input = secret word (full answer) (ELSEIF)
+  #Make sure that both lower and upper case letters are acceptable and treated as equivalent
+  #Congratulation prompt
+  #Provide the user with a visual clue of how their progress - _ _ _ _ with correct letters 
+  #Always notify the user about the correct letters/wrong letters, remaining tries.
+  #OR
+  #If full answer input is not secret word, notify user that they've lost. Reveal secret 
+  #and exit. (This will bring it back to the while loop which will break when # of 
+  #wrong guesses = max wrong guesses)
+
+#7c. ELSE - invalid input try again
+  #Always notify the user about the correct letters/wrong letters, remaining tries.
+  #Provide the user with a visual clue of how they are progressing - update _ _ _ _ with 
+  #correct guessed letters.
 
 ##DISPLAY a hangman like game, have the _ _ _ _ spaces for each letter, have a wrong guesses
 #section - MAKE IT FUN! 
@@ -72,7 +90,7 @@ wrong_guesses_list <- c()
 #correctly guessed letter tracker 
 guessed_word <- rep("_", secret_word_length)
 
-#Introductions to the game, character length of word, and rules 
+#introductions to the game, character length of word, and rules 
 cat("Welcome to the game of hangman! Let's \"hang\" and play a quick game! \n
 Your secret word has been chosen and the length of the secret word is", 
     secret_word_length, "characters long. \nYou're allowed up to", 
@@ -80,6 +98,63 @@ Your secret word has been chosen and the length of the secret word is",
     "wrong guesses in this game.\nYou can enter one letter or if you think you know the word, you can enter the full word. \n
 Good luck! We will be watching.")
 
+#while loop is the main game play loop. The loop will break when the players makes 6 wrong
+#guesses or they guess the full word correctly or incorrectly
+while(wrong_guesses < max_wrong_guesses) {
 
+#if number of wrong guesses = max wrong guesses or user_input = secret word,
+#break the loop and exit
+  if(wrong_guesses == max_wrong_guesses | user_input == secret_word) {
+    if(wrong_guesses == max_wrong_guesses){
+      cat("Unfortunately, you have used up all of your guesses. You have been hanged. \nWOMP WOMP! The answer was \"",
+        secret_word, "\". Better luck next time.")
+    }
+    else{
+      cat("Congratulations! You've gusses the right word! The answer was", secret_word, "\nand you answered it with", 
+          max_wrong_guesses - wrong_guesses, "guesses left. You didn't get hanged today! Slay!")
+    }
+    break
+  }
+  
+#display the current state of the game
+#let the users know the letters the guessed correctly, wrong letters guessed, and 
+#the number of guesses they have left
 
+  
+  
+}
 
+#6. Ask for user input.
+#7a. Check System for one character (IF)
+#Check if the character is a letter
+#Make sure that both lower and upper case letters are acceptable and treated as equivalent
+#SIDE NOTE - create a vector to keep track of correctly guessed letters
+#8a. Check to see if the user input is in the secret word. (correct)
+#If yes, notify user and ask for next letter or guess the whole word
+#Always notify the user about the correct letters/wrong letters, remaining tries.
+#Provide the user with a visual clue of how they are progressing - update _ _ _ _ with 
+#correct guessed letters 
+#If not, notify user.
+#If user has not exhausted the available tries repeat.
+#Always notify the user about the correct letters/wrong letters, remaining tries.
+#Provide the user with a visual clue of how they are progressing - update _ _ _ _ with 
+#correct guessed letters 
+#OR 
+#If tries are exhausted, notify user that they’ve lost. Reveal secret and exit. (This 
+#will bring it back to the while loop which will break when # of wrong guesses = max 
+#wrong guesses)
+
+#7b. Check system if user input = secret word (full answer) (ELSEIF)
+#Make sure that both lower and upper case letters are acceptable and treated as equivalent
+#Congratulation prompt
+#Provide the user with a visual clue of how their progress - _ _ _ _ with correct letters 
+#Always notify the user about the correct letters/wrong letters, remaining tries.
+#OR
+#If full answer input is not secret word, notify user that they've lost. Reveal secret 
+#and exit. (This will bring it back to the while loop which will break when # of 
+#wrong guesses = max wrong guesses)
+
+#7c. ELSE - invalid input try again
+#Always notify the user about the correct letters/wrong letters, remaining tries.
+#Provide the user with a visual clue of how they are progressing - update _ _ _ _ with 
+#correct guessed letters.
